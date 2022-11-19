3da7525164ed6a6cbf66591caeece8ad724c77a5
bintangaugryf <116374447+bintangaugryf@users.noreply.github.com>
2022-11-19 10:25:34 +0700

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,9 @@
+    public class  Perkenalan {
+    public static void main(String[] args) {
+	System.out.println("Assalamualaikum");
+        String nama = "BINTANG AUGRI FARIS";
+        String tanggal_lahir = "19";
+        String bulan_lahir = "Agustus" ;
+        int tahun_lahir = 2002 ;
+        System.out.println("Nama Saya :" + nama +, "Saya lahir di tahun :" +, tahun_lahir);
+    }		
